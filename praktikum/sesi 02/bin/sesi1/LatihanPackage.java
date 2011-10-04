17a209d280cddb601e7e3219734187807fbc009c
sony <sonday_182@yahoo.com>
2011-10-04 19:44:19 +0700

    file praktikum sesi 4

---
+++ praktikum/sesi 02/bin/sesi1/LatihanPackage.java
@@ -0,0 +1,8 @@
+package sesi1;
+
+public class LatihanPackage{
+	public static void main(string[] x){
+		system.out.println("sesi 1");
+	}
+}
+	
---
+++ praktikum/sesi 02/src/praktikum.java
@@ -0,0 +1,15 @@
+public class praktikum{
+	public static void main(String[] args){
+int x = 10;
+long y = x;
+
+long a = 5;
+int b = (int) a;
+
+float x = 10;
+double y = x;
+
+double a = 5;
+float b = (float) a;
+}
+}
